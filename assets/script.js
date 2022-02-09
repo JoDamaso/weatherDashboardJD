c7512de9262e8b5ca7d253f448e7597b5ed6421b
JoDamaso <darryldamaso@gmail.com>
2022-02-09 02:02:15 -0600

    dynamically created the searched city

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,89 @@
+// API Key
 const apiKey = "b791ce644df21a24637c0d54506688a5";
 
-
+// these variables target the search button, history, and inputted city 
 var searchBtn = document.getElementById('search');
 var history = document.getElementById('history');
-var cityName = document.getElementById('city');
+var city = document.getElementById('searched-city');
 
+// these variables target the current searched city 
+var currentCity = document.getElementById('current-city');
+var currentTemp = document.getElementById('temp');
+var currentWind = document.getElementById('wind');
+var currentHumid = document.getElementById('humidity');
+var currentUv = document.getElementById('uv');
+var currentIcon = document.getElementById('dashboard-icon');
+var dashBoardEl = document.getElementById('city-weather');
+var fiveDay = document.getElementById('forecast');
+var dailyWeather = document.getElementById('future-weather');
 
-function finder () {
+// current city and inserting temp, wind, humidity, uv into the current searched city
+function cityFinder () {
+    console.log(city.value);
     limit = 5;
-    // let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&limit=${limit}&appid=${apiKey}`;
-    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&appid=${apiKey}`;
-    fetch(apiUrl)
+    let cityUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city.value}&appid=${apiKey}`;
+
+    fetch(cityUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        console.log(data)
+        // console.log(data.city.coord);
+        const {lat, lon} = data.city.coord;
+        let currentUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&limit=${limit}&units=imperial&appid=${apiKey}`;
+        fetch(currentUrl)
+        .then(function (response) {
+            dashBoardEl.classList.remove("d-none");
+            fiveDay.classList.remove("d-none");
+            dailyWeather.classList.remove("d-none");
+            return response.json();
+        })
+        .then(function (data) {
+            const {temp, uvi, humidity, wind_speed} = data.current;
+            var dashBoardIcon = data.current.weather[0].icon
+            currentTemp.textContent = temp + " °F";
+            currentHumid.textContent = humidity + " %";
+            currentWind.textContent = wind_speed + " MPH";
+            currentUv.textContent = uvi;
+            currentCity.textContent = city.value;
+            currentIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + dashBoardIcon + "@2x.png");
+            currentIcon.setAttribute("alt", data.current.weather[0].description);
+            console.log(data)
+        })
+        .then(function (data) {
+        var fiveHumidity  = data.daily[0].humidity;
+        var fiveWindSpeed = data.daily[0].wind_speed;
+        var fiveTemp = data.daily[0].temp.day;
+        var futureIcon = data.daily[0].weather[0].icon;
+        console.log(data.daily[0].temp.day);
+        })
     })
     .catch(function (error) {
+        alert("Please enter a city name located in the U.S");
         console.log("not working", error);
     })
 };
 
-finder();
-// searchBtn.addEventListener('click', function() {
-//     var displayCity = searchBtn.value.trim();
-//     displayCity.appendChild(cityName);
+searchBtn.addEventListener("click", cityFinder);
+
+// var fiveHumidity  = data.daily[0].humidity;
+// var fiveWindSpeed = data.daily[0].wind_speed;
+// var fiveTemp = data.daily[0].temp.day;
+// var futureIcon = data.daily[0].weather[0].icon;
+// console.log(data.daily[0].temp.day);
+
+// for 5 day forcast 
+// const d = new Date(1644343200 * 1000);
+// let text = d.toUTCString();
+// d.toLocaleString(locale, {
+//     timeZone: "America/Chicago",
+//     weekday: "short",
+//     day: "numeric",
+//     month: "short",
+//     year: "numeric",
+//     hour: "numeric",
+//     minute: "numeric",
+//     timeZoneName: "short",
 // });
 
 
